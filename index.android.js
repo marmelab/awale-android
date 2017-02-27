88ecbec0ea5be1037e730b9bf8198a084d2dc12c
zyhou <richard.maxime9@gmail.com>
2017-02-27 10:34:13 +0100

    pure function

--- index.android.js
+++ index.android.js
@@ -1,12 +1,10 @@
 import { AppRegistry, Text } from 'react-native';
 import React from 'react';
 
-export default class App extends React.PureComponent {
-    render() {
-        return (
-            <Text>oui</Text>
-        );
-    }
-}
+const App = () => (
+    <Text>oui</Text>
+ );
+
+export default App;
 
 AppRegistry.registerComponent('awaleAndroid', () => App);
