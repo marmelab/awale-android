cbe8ebcca1677494b12dae5af74807f48ee3e8d9
zyhou <richard.maxime9@gmail.com>
2017-02-27 10:09:36 +0100

    export protypes board.js

--- src/awale/board/Board.js
+++ src/awale/board/Board.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -45,5 +45,5 @@ export default class Board extends Component {
 }
 
 Board.propTypes = {
-    board: React.PropTypes.arrayOf(React.PropTypes.number).isRequired,
+    board: PropTypes.arrayOf(React.PropTypes.number).isRequired,
 };
