abe9507ae1367bec0885b3eed62dbf9038ca45cc
zyhou <richard.maxime9@gmail.com>
2017-03-01 17:58:34 +0100

    rename comment on test board

--- __tests__/app/pitButton.js
+++ __tests__/app/pitButton.js
@@ -4,13 +4,13 @@ import { Text } from 'react-native';
 import PitButton from '../../src/app/board/PitButton';
 
 describe('<PitButton />', () => {
-    it('should render stuff', () => {
+    it('should render One', () => {
         const pitButton = shallow(<PitButton onPress={() => {}} pitValue={0} pitIndex={0} />);
         expect(pitButton.length).toEqual(1);
         expect(pitButton.find(Text).props().children).toEqual(0);
     });
 
-    it('should render One', () => {
+    it('should render Two', () => {
         const pitButton = shallow(<PitButton onPress={() => {}} pitValue={1} pitIndex={0} />);
         expect(pitButton.length).toEqual(1);
         expect(pitButton.find(Text).props().children).toEqual(1);
